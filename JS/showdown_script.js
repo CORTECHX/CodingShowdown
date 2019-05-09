b5009e3a831e8df16dacc49a507ba2ccfa12ea94
root <root@localhost.localdomain>
2019-05-09 00:50:45 -0500

    Modified code

---
+++ JS/showdown_script.js
@@ -0,0 +1,6 @@
+var url_string = window.location.href;
+var url = new URL(url_string);
+var id = url.searchParams.get("id"); //id of the match to be displayed
+console.log(c);
+
+///get the video feeds from firebase and display the match
